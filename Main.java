bacb027e76fe90c02d0b988f1b33102400b58cb6
OscarLizano001 <98754897+Oscarlizano001@users.noreply.github.com>
2023-02-28 02:28:58 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main {
+    public int length(){
+    return value.length;
+  }
+  public static void main(String[] args) {
+    System.out.println("Enter a word: ");
+    // public int length();
+    Scanner scan = new Scanner(System.in);
+    String UserInput = scan.next();
+
+    System.out.println(UserInput);
+
+    int length = name.length();
+    
+    
+  }
+
+}
